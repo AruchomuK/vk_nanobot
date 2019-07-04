4a8b0cb7279b01e01005fc287557234eea835ad1
AruchomuK <fightingman2001@gmail.com>
2019-07-04 17:20:56 +0500

    commit_2

--- first_bot.py
+++ first_bot.py
@@ -11,3 +11,23 @@ def write_msg(user_id, text):
                         'random_id' : random.randint(0,2147483648),
                         'message' : text})
 write_msg(USER_ID, 'Hey')
+
+lp_server = vkBot.method('messages.getLongPollServer',
+                         {'lp_version' : 3,
+                         'need_pts' : 0})
+print(lp_server)
+
+ts,key,server = lp_server['ts'], lp_server['key'], lp_server['server']
+
+print("Готов к работе")
+
+
+while True:
+    long_poll = requests.get(
+        'https://{srver}?act={act}&key={key}&ts={ts}&wait=500'.format(server=server,
+                                                                      act='a_check',
+                                                                      key=key,
+                                                                      ts=ts)).json()
+    update = long_poll['updates']
+
+    ts = long_poll['ts']
