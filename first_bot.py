1f9bf1ebb996f637e7760405f02b1b7cdb2748d5
AruchomuK <fightingman2001@gmail.com>
2019-07-04 16:44:30 +0500

    commit_1

--- first_bot.py
+++ first_bot.py
@@ -2,11 +2,12 @@ import vk_api
 import random
 from config import *
 
-vkBot = vk_api.VkApi(token=BOT_TOKEN)
+vkBot = vk_api.VkApi(token=ACCESS_TOKEN)
 
 USER_ID = 446927861
-text = 'Это сообщене отправлено с помощью vk_api '
-result = vkBot.method('messages.send',
+def write_msg(user_id, text):
+    vkBot.method('messages.send',
                        {'user_id' : USER_ID,
-                        'random_id' : random.randint(1,2147483648),
+                        'random_id' : random.randint(0,2147483648),
                         'message' : text})
+write_msg(USER_ID, 'Hey')
